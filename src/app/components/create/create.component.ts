049596ea18cf211de7d330d93d688a8544e124ab
edilsonlonC <eddylson.londonio@gmail.com>
2018-12-03 16:29:03 -0500

    images load -error

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {Project} from '../../models/project';
 import { ProjectService} from '../../services/project.service';
-
+import  { UploadService } from "../../services/upload.service";
+import {Global} from "../../services/global";
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService , UploadService]
 })
 export class CreateComponent implements OnInit {
   public title: string;
   public project : Project;
   public status : boolean;
+  public filesToUpload: Array<File>;
   constructor(
+      private _uploadService: UploadService,
       private _projectService: ProjectService
 
   ) {
@@ -23,19 +26,31 @@ export class CreateComponent implements OnInit {
   ngOnInit() {
   }
   onSubmit(form){
-    console.log(this.project);
+    // guardar los datos
     this._projectService.saveProject(this.project).subscribe(
         response => {
-          if (response.project) this.status=true;
-          else this.status = false;
+          if (response.project) {
+            this.status=true;
+
+            // subir la imagen
+            this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id,[],this.filesToUpload,'image')
+            .then((result:any) => {
+            console.log(result);
+            this.status=true;
+              form.reset();
+          });
+          }else this.status = false;
         },
 
         error => {
-          console.log(error);
+          console.log("<any>error)");
         }
 
 
     )
+  }
+  fileChangeEvent (fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
